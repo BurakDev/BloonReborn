392fe7e4479e4bb70b03f895601bb1fdcbaff25c
Burak <burak@burak.fr>
2016-10-04 19:20:09 +0200

    Implement Configuration Manager

--- src/Emulator.ts
+++ src/Emulator.ts
@@ -4,13 +4,16 @@
 /// <reference path="refs/pathfinding.d.ts" />
 import GameServer from './Networking/GameServer';
 import Logging from './Core/Logging';
+import ConfigurationManager from './Core/ConfigurationManager';
 
 export default class Emulator {
     private static gameServer: GameServer;
     private static logging: Logging;
+    private static configurationManager: ConfigurationManager;
 
     public static main(): void {
-        Emulator.gameServer = new GameServer("127.0.0.1", 1232);
+        Emulator.configurationManager = new ConfigurationManager('../config.ini');
+        Emulator.gameServer = new GameServer(Emulator.getConfigurationManager().getValue('game.host', '127.0.0.1'), Emulator.getConfigurationManager().getInt('game.port', 1232));
         Emulator.gameServer.initialise();
         Emulator.gameServer.connect();
         Emulator.logging = new Logging();
@@ -22,6 +25,10 @@ export default class Emulator {
 
     public static getLogging(): Logging {
         return this.logging;
+    }
+
+    public static getConfigurationManager(): ConfigurationManager {
+        return this.configurationManager;
     }
 }
 
