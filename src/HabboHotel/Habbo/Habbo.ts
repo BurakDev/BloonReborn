aedb0b26e46b90a0e3f6aabe19f5e0b921921617
Burak <burak@burak.fr>
2016-10-07 17:34:50 +0200

    Work on Habbo, HabboManager

--- src/HabboHotel/Habbo/Habbo.ts
+++ src/HabboHotel/Habbo/Habbo.ts
@@ -1,3 +1,12 @@
 export default class Habbo {
+    private id: number;
+    private username: string;
 
+    public getId(): number {
+        return this.id;
+    }
+
+    public getUsername(): string {
+        return this.username;
+    }
 }
--- src/HabboHotel/Habbo/HabboManager.ts
+++ src/HabboHotel/Habbo/HabboManager.ts
@@ -1,3 +1,34 @@
-export default class HabboManager {
+import Habbo from './Habbo';
+import GameClient from '../../Networking/GameClient';
 
+export default class HabboManager {
+    private onlineHabbos: Array<Habbo>;
+
+    public constructor() {
+        this.onlineHabbos = new Array<Habbo>();
+    }
+
+    public removeHabbo(habbo: Habbo): void {
+        this.onlineHabbos.slice(habbo.getId(), 1);
+    }
+
+    public getHabbo(search: number | string): Habbo {
+        if (typeof search == 'number') {
+            return this.onlineHabbos[<number>search];
+        } else if (typeof search == 'string') {
+            this.onlineHabbos.forEach(function(habbo: Habbo) {
+                if (search.toString().toLowerCase() === habbo.getUsername().toLowerCase()) {
+                    return habbo;
+                }
+            });
+        }
+    }
+
+    public authByTicket(sso: string, client: GameClient, cb: (client: GameClient, habbo: Habbo) => void): void {
+
+    }
+
+    public authByLogin(username: string, password: string, client: GameClient, cb: (client: GameClient, habbo: Habbo) => void): void {
+
+    }
 }
